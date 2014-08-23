1a55968fe6b1e3acdb2fe0e3e9e3a32d46fa8077
mlb21009a <mlb21009a@gmail.com>
2014-08-23 14:49:10 +0900

    Initial Commit

---
+++ HowToSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HowToSwift
+//
+//  Created by R on 2014/08/23.
+//  Copyright (c) 2014年 マック太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
