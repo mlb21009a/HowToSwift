624cc0da40b674da78ca6d784212a1d94fbf9086
mlb21009a <mlb21009a@gmail.com>
2026-03-11 16:34:01 +0900

    appDelegate修正

--- HowToSwift/AppDelegate.swift
+++ HowToSwift/AppDelegate.swift
@@ -14,8 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    private func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         return true
     }
 
